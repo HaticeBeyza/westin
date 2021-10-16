8a2a83ced1f190caf7312cfb7ac2f9df989d9446
Hatice Beyza <haticebeyzacamur@gmail.com>
2021-10-16 15:57:37 +0300

    v1

---
+++ src/components/Preloader.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Preloader = () => {
+  return (
+    /* Preloader Start */
+    <div id="angela-preloader">
+      <div className="preloader">
+        <div className="spinner"></div>
+        <div className="loader">
+          <span data-text="W" className="letter-animation">
+            W
+          </span>
+          <span data-text="E" className="letter-animation">
+            E
+          </span>
+          <span data-text="S" className="letter-animation">
+            S
+          </span>
+          <span data-text="T" className="letter-animation">
+            T
+          </span>
+          <span data-text="I" className="letter-animation">
+            I
+          </span>
+          <span data-text="N" className="letter-animation">
+            N
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Preloader;
+/* components olusturma */
